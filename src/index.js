68795a5b9860b968dec9db18b238d1c8cf1514e9
Bharat Kumawat <bharatkumawatk1@gmail.com>
2024-09-30 00:09:08 +0530

    First commit with basic setup

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+const { PORT } = require("./config/serverConfig");
+const apiRoutes = require('./routes/index');
+
+const setupAndStartServer = () => {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use('/api', apiRoutes);
+
+  app.listen(PORT, () => {
+    console.log(`server started at PORT ${PORT}`);
+  });
+};
+
+setupAndStartServer();
